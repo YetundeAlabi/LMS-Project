e7639ef5571328c7a7266c5d23549f8600287123
YetundeAlabi <alabiyetunde007@gmail.com>
2023-05-31 15:09:32 +0100

    user proxy model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,8 @@
+import uuid
+
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, AbstractUser
+from django.db.models.query import QuerySet
 
 
 class MyUserManager(BaseUserManager):
@@ -24,19 +27,25 @@ class MyUserManager(BaseUserManager):
 
 class User(AbstractUser):
     """ Custom user model that supports using email instead of username"""
-    ADMIN = 0
-    TUTOR = 1
-    STUDENT = 2
-    ROLE_CHOICES = [
-        (ADMIN, "Admin"),
-        (TUTOR, "Tutor"),
-        (STUDENT, "Student")
-    ]
+
+    class Role (models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        TUTOR = "TUTOR", "Tutor"
+        STUDENT = "STUDENT", "Student"
     
+    base_role = Role.ADMIN 
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     email = models.EmailField(max_length=255, unique=True, verbose_name="email address")
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, default=ADMIN)
+    last_login = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    role = models.CharField(max_length=50, choices=Role.choices)
+
+    is_student = models.BooleanField(default=False)
+    is_tutor = models.BooleanField(default=False)
 
     REQUIRED_FIELDS = []
     USERNAME_FIELD = "email"
@@ -46,29 +55,56 @@ class User(AbstractUser):
     def __str__(self):
         return self.email 
     
-
-class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    last_login = models.DateTimeField(auto_now=True)
-    is_verified = models.BooleanField(default=False)
-    is_suspended = models.BooleanField(default=False)
-    picture = models.ImageField(upload_to='accounts/media', blank=True)
-    is_deleted = models.BooleanField(default=False)
-    
     def get_full_name(self) -> str:
         return f'{self.first_name} {self.last_name}'
     
-    def __str__(self):
-        return self.user.email
+    def save(self, *args, **kwargs):
+        if not self.role or self.role == None:
+            self.role = self.base_role
+            self.is_admin = True
+            return super().save(*args, **kwargs)
 
 
-class Tutor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    is_verified = models.BooleanField(default=False)
-    is_suspended = models.BooleanField(default=False)
-    picture = models.ImageField(upload_to='accounts/media', blank=True)
-    is_deleted = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.user.email
+class StudentManager(BaseUserManager):
     
+    def get_queryset(self, *args, **kwargs):
+        queryset =  super().get_queryset(*args, **kwargs)
+        return queryset.filter(role=User.Role.STUDENT)
+
+
+class Student(User):
+    base_role = User.Role.STUDENT
+    objects = StudentManager()
+    
+    class Meta:
+         db_table = 'student'
+
+    def save(self, *args, **kwargs):
+        self.role = User.Role.STUDENT
+        self.is_student = True
+        return super().save(*args, **kwargs)
+    
+    
+    
+    def __str__(self):
+        return self.base_role
+    
+
+class TutorManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results =  super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.TUTOR)
+
+
+class Tutor(User):
+    base_role = User.Role.TUTOR
+    # objects = TutorManager()
+    class Meta:
+        proxy = True
+
+    def save(self, *args, **kwargs):
+        self.role = User.Role.TUTOR
+        self.is_tutor = True
+        return super().save(*args, **kwargs)
+    
+
