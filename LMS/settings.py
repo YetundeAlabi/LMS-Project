783b14ee6d783a218160567313fea57264143c18
Your Name <otutaofeeqi@gmail.com>
2023-06-14 17:15:02 +0100

    updates on student

--- LMS/settings.py
+++ LMS/settings.py
@@ -22,7 +22,6 @@ TEMPLATES_DIR = BASE_DIR / 'LMS'/ 'templates'
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-SECRET_KEY= 'django-insecure-$$z=kt(08)a)%q11q@@=#gt5^q+v4^p2!c@x@x@r42h6$wb^'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
