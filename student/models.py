79696fbaa40f11532ae3210cf6c0c4421a9de9f2
Your Name <otutaofeeqi@gmail.com>
2023-06-14 11:17:51 +0100

    added slugs to student models

--- student/models.py
+++ student/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.db.models import Sum
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.text import slugify
 from tutor.models import Student, Course, Topic, SubTopic, Track
 
 
@@ -9,6 +12,7 @@ class StudentCourse(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     track = models.ForeignKey(Track, on_delete=models.CASCADE, null=True, blank=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True, null=True, blank=True)
     progress_level = models.FloatField(default=0.0)
 
     def update_progress_level(self):
@@ -26,6 +30,7 @@ class StudentCourse(models.Model):
 class StudentTopic(models.Model):
     student_course = models.ForeignKey(StudentCourse, on_delete=models.CASCADE, related_name='student_topics')
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True, null=True, blank=True)
     progress_level = models.FloatField(default=0.0)
 
     def update_progress_level(self):
@@ -43,6 +48,7 @@ class StudentTopic(models.Model):
 class StudentSubTopic(models.Model):
     student_topic = models.ForeignKey(StudentTopic, on_delete=models.CASCADE, related_name='student_subtopics')
     sub_topic = models.ForeignKey(SubTopic, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True, null=True, blank=True)
     progress_level = models.FloatField(default=0.00)
 
     def update_progress_level(self):
@@ -52,4 +58,27 @@ class StudentSubTopic(models.Model):
 
     def __str__(self):
         return f"student subtopic {self.id} , under {self.student_topic}"
+
+
+@receiver(post_save, sender=StudentCourse)
+def student_course_slug(sender, instance, created, **kwargs):
+    if created and not instance.slug:
+        slug = slugify(instance.course.title)
+        instance.slug = slug
+        instance.save()
+
+@receiver(post_save, sender=StudentTopic)
+def student_topic_slug(sender, instance, created, **kwargs):
+    if created and not instance.slug:
+        slug = slugify(instance.topic.title)
+        instance.slug = slug
+        instance.save()
+
+@receiver(post_save, sender=StudentSubTopic)
+def student_subtopic_slug(sender, instance, created, **kwargs):
+    if created and not instance.slug:
+        slug = slugify(instance.sub_topic.title)
+        instance.slug = slug
+        instance.save()
+
 
