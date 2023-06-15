27ba5c61e54569a4714cbe2eaef0b44a6286a018
YetundeAlabi <alabiyetunde007@gmail.com>
2023-06-15 14:41:53 +0100

    added student landing page

--- student/urls.py
+++ student/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
+
 from . import views
 
-
 app_name='student'
-
-
-
 urlpatterns = [
     path('student_courses/', views.StudentCourseListView.as_view(), name='course_list'),
     path('student_courses/<slug:student_course_slug>/student_topics/', views.StudentTopicListView.as_view(), name='topic_list'),
